0e8bae6a44a3d79865a25d894918fba93932d1e4
Markus Schlenker <markus_schlenker@gmx.de>
2026-03-27 15:20:18 +0100

    Update docker-compose-demo

--- week3-docker-sqlalchemy/day5/docker-compose-demo/app/app.py
+++ week3-docker-sqlalchemy/day5/docker-compose-demo/app/app.py
@@ -1,5 +1,18 @@
+import time
+
+
+def main_long():
+    for i in range(10):
+        print(f"{i} - Hello from Docker + Docker compose + Custom Image! It is now {time.localtime()}")
+        time.sleep(3)
+
 def main():
+    time.sleep(2)
     print("Hello from Docker + Docker compose + Custom Image!")
+    time.sleep(2)
+    print("Hello from Docker + Docker compose + Custom Image!")
+    time.sleep(2)
 
 if __name__ == "__main__":
     main()
+    #main_long()
