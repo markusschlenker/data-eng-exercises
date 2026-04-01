9a8e6636cd533e3a14734f1a13b8f991997dc4c6
Markus Schlenker <markus_schlenker@gmx.de>
2026-04-01 20:50:36 +0200

    Refactor app

--- week3-docker-sqlalchemy/day5/activity2/app/app.py
+++ week3-docker-sqlalchemy/day5/activity2/app/app.py
@@ -1,5 +1,13 @@
+from pathlib import Path
+
 def main():
     print("Hello from Docker + Docker compose + Custom Image!")
+    print(f"{Path.cwd()=}")
 
+    with open("data/northwind_UTF8.csv", "r") as file:
+        data = file.readlines()
+        print("Data from northwind_UTF8.csv:")
+        print(data[:3])  # print first 3 lines for brevity
+    
 if __name__ == "__main__":
     main()
