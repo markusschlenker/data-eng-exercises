6b3af9eb64b462518098140c5608db052ec649f9
Markus Schlenker <markus_schlenker@gmx.de>
2026-04-13 17:06:37 +0200

    Fix bug for activity 2

--- week5-orchestration/02-dbt/sf_dbt_activity2/models/marts/fct_debug.sql
+++ week5-orchestration/02-dbt/sf_dbt_activity2/models/marts/fct_debug.sql
@@ -1,6 +1,7 @@
 with perf as (
     select * from {{ ref('fct_seller_performance') }}
 )
-select * 
+select *, cnt_orders - cnt_orders_dist as cnt_order_dev
 from perf
-where cnt_orders_dist != cnt_orders_dist
+where cnt_orders_dist != cnt_orders
+order by cnt_order_dev DESC
--- week5-orchestration/02-dbt/sf_dbt_activity2/models/marts/fct_seller_performance.sql
+++ week5-orchestration/02-dbt/sf_dbt_activity2/models/marts/fct_seller_performance.sql
@@ -9,6 +9,6 @@ select
     seller_id,
     sum(price) as total_sales_dollar,
     count(order_id) as cnt_orders,
-    count(DISTINCT order_id) as cnt_orders_dist  -- in case multiple items from one order_id - not the case here
+    count(DISTINCT order_id) as cnt_orders_dist
 from sales
 group by seller_id
